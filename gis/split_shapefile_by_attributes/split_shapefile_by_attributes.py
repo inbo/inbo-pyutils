80ad975a56e7ea7136c58b07be05a0587821b347
stijnvanhoey <stijnvanhoey@gmail.com>
2017-06-08 16:46:43 +0200

    Add field selection to GUI

--- gis/split_shapefile_by_attributes/split_shapefile_by_attributes.py
+++ gis/split_shapefile_by_attributes/split_shapefile_by_attributes.py
@@ -2,28 +2,78 @@
 import os
 import sys
 
+#import tkinter
+from tkinter import Tk
 import tkinter.filedialog as tkfd
 import tkinter.simpledialog as tksd
 
 import fiona
 
+
+class AttributeSelect(Tk):
+
+    def __init__(self, parent, options):
+        Tk.__init__(self, parent)
+        self.initialize(options)
+        self.grid()
+        self.field = None
+
+    def initialize(self, options):
+
+        # Dropdown Menu
+        self.dropVar = tksd.StringVar()
+        self.dropVar.set(options[0]) # default choice is first field name
+        self.dropMenu1 = tksd.OptionMenu(self, self.dropVar, *options, command=self.func)
+        self.dropMenu1.grid(column=1,row=2)
+        self.CloseButton = tksd.Button(self, text = "Start splitting", command = self.close_window)
+        self.CloseButton.grid(column=1, row=4)
+
+    def func(self,value):
+        self.field = value
+
+    def close_window(self):
+        self.destroy()
+
+def create_field_form(options):
+    form = AttributeSelect(None, options)
+    form.title('Select the field to split the shapefile')
+    form.mainloop()
+    print("Field used for splitting is", form.field)
+    return(form.field)
+
+
 def gui_inputs():
     """
     """
     # locate your input shapefile
+    print("select your input file")
+    root = Tk()
     infile = tkfd.askopenfilename(title = "Select your shapefile", 
-                                initialdir = "C:/", 
-                                filetypes = (("shapefile", "*.shp"), 
+                                  initialdir = "C:/", 
+                                  filetypes = (("shapefile", "*.shp"), 
                                              ("all files", "*.*")))
-
+ 
     # define destination -folder- where output -shapefiles- will be written
+    print("select your output dir")
     dest = tkfd.askdirectory(title = "Select your output folder", 
                             initialdir = "C:/")
+    root.destroy()
 
-    # define field to split by attributes
-    field = tksd.askstring("Field", "Enter the name of the field to conduct the split by attributes on")
+    # choose field to split by attributes
+    print("Select your field name")
+    field_options = _extract_attribute_field_names(infile)
+    field = create_field_form(field_options)
+    print("selection done")
 
     return infile, field, dest
+
+def _extract_attribute_field_names(infile):
+    """extract the field names from a fiona compatible vector file
+    """
+    with fiona.open(infile) as source:
+        field_names = source.meta["schema"]["properties"].keys()
+
+    return list(field_names)
 
 
 def split_shape_by_attribute(infile, field, dest):
@@ -56,12 +106,12 @@ def split_shape_by_attribute(infile, field, dest):
 def main(argv=None):
     """
     """     
-
+    print("starting GUI...")
     infile, field, dest = gui_inputs()
     
     print("Start splitting procedure...")
     split_shape_by_attribute(infile, field, dest)
-    print("... slitting completed!")
+    print("... splitting completed!")
 
 
 if __name__ == "__main__":
